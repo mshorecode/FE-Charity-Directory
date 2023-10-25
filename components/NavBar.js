17cf5d951d97068990551b97876ce142ba40d918
Ryan Shore <mshorecode@gmail.com>
2023-10-24 21:10:49 -0500

    updated navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,34 +2,23 @@
 import React from 'react';
 import Link from 'next/link';
 import {
-  Navbar, Container, Nav, Button,
+  Navbar, Nav, Button,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+      <Link passHref href="/">
+        <Navbar.Brand style={{ margin: '0px 10px 0px 20px' }}>Charity Directory</Navbar.Brand>
+      </Link>
+      <Nav className="me-auto">
+        {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
+        <Link passHref href="/charities/view">
+          <Nav.Link>Charities</Nav.Link>
         </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/charities/view">
-              <Nav.Link>Charities</Nav.Link>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      <Button variant="danger" onClick={signOut}>Sign Out</Button>
+      </Nav>
+      <Button variant="danger" onClick={signOut} style={{ margin: '0px 20px 0px 0px' }}>Sign Out</Button>
     </Navbar>
   );
 }
