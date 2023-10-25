8f5e331dcf9ad47ffc079c8542875774ffa524b3
Ryan Shore <mshorecode@gmail.com>
2023-10-24 21:03:25 -0500

    removed uneccessary js files, created charity details page, fixed edit form

---
+++ pages/charities/edit/[id].js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSingleCharity } from '../../../api/charityData';
+import CreateCharityForm from '../../../components/CreateForm';
+
+export default function EditCharity() {
+  const [editItem, setEditItem] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    getSingleCharity(id).then(setEditItem);
+  }, [id]);
+
+  return (<CreateCharityForm chObj={editItem} />);
+}
